1aa3af30cedb098616e8e351ab0224988559db4f
Ivan Vukusic <jaaajaa102@gmail.com>
2024-04-02 00:31:55 -0400

    Move over everything from josean-dev

--- after/plugin/netrw.lua
+++
@@ -1,6 +0,0 @@
-local o = vim.opt
-
-o.colorcolumn = nil
-
--- Line numbers etc in Netrw
-vim.cmd([[let g:netrw_bufsettings = 'noma nomod nu nobl nowrap ro']])
---
+++ lua/vonkoff/core/init.lua
@@ -0,0 +1,3 @@
+require("vonkoff.core.options")
+require("vonkoff.core.keymaps")
+
